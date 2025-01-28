60947a8149dcac93204952920baef95811aa1797
acan12 <ary.suryawan@ottodigital.id>
2025-01-28 22:43:31 +0700

    update proj structure

--- coconut2-mvvm/src/main/java/app/coconut2/coconut2_mvvm/base/ui/BaseActivity.kt
+++ coconut2-mvvm/src/main/java/app/coconut2/coconut2_mvvm/base/ui/BaseActivity.kt
@@ -4,17 +4,29 @@ import android.content.ComponentCallbacks2
 import androidx.appcompat.app.AppCompatActivity
 import app.coconut2.coconut2_mvvm.base.remote.BaseResponse
 import app.coconut2.coconut2_mvvm.base.remote.ErrorResponse
-import app.coconut2.coconut2_mvvm.interfaces.IView
+import app.coconut2.coconut2_mvvm.interfaces.IApiResponse
 
-open class BaseActivity : AppCompatActivity(), IView,
-    ComponentCallbacks2 {
+open class BaseActivity : AppCompatActivity(), IApiResponse, ComponentCallbacks2 {
+    override fun handleSuccess(response: BaseResponse?) {
+        TODO("Not yet implemented")
+    }
 
-    override val currentActivity: BaseActivity
-        get() = this
+    override fun handleError(message: String?) {
+        TODO("Not yet implemented")
+    }
 
-    override fun handleSuccess(response: BaseResponse?) {}
-    override fun handleError(message: String?) {}
-    override fun handleError(response: ErrorResponse?) {}
-    override fun handleNoConnectionInternet() {}
-    override fun callbackReConnectingNetwork() {}
+    override fun handleError(response: ErrorResponse?) {
+        TODO("Not yet implemented")
+    }
 }
+
+
+
+//    override val currentActivity: BaseActivity
+//        get() = this
+//
+//    override fun handleSuccess(response: BaseResponse?) {}
+//    override fun handleError(message: String?) {}
+//    override fun handleError(response: ErrorResponse?) {}
+//    override fun handleNoConnectionInternet() {}
+//    override fun callbackReConnectingNetwork() {}
