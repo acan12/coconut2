a7744039e38a72be1c21202108500712e25c7e10
acan12 <ary.suryawan@ottodigital.id>
2025-03-29 14:02:31 +0700

    update api architecture when get data repository with mvvm methodology

--- coconut2-mvvm/src/main/java/app/coconut2/coconut2_mvvm/core/ui/NetworkState.kt
+++
@@ -1,7 +0,0 @@
-package app.coconut2.coconut2_mvvm.core.ui
-
-sealed class NetworkState {
-    object Loading : NetworkState()
-    data class Success<T>(val data : T) : NetworkState()
-    data class Error(val message: String) : NetworkState()
-}
